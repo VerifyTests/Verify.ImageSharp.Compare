0265616db4f511f22515c1e4674f8611ed5d30c3
Simon <simon.cropp@gmail.com>
2023-01-09 12:50:17 +1100

    docs

--- src/Tests/ModuleInitializer.cs
+++ src/Tests/ModuleInitializer.cs
@@ -1,14 +1,14 @@
-﻿public static class ModuleInitializer
+﻿static class ModuleInitializer
 {
     #region enable
 
     [ModuleInitializer]
-    public static void Init()
-    {
+    public static void Init() =>
         VerifyImageSharpCompare.Initialize();
 
-        #endregion
+    #endregion
 
+    [ModuleInitializer]
+    public static void InitOther() =>
         VerifyDiffPlex.Initialize();
-    }
 }
